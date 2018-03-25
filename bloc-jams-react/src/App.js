cda5db251471d748caf6aafce23b5f68153a95e4
Ashley Morgan Nowakowski <amn0220@yahoo.com>
2018-03-24 19:05:37 -0700

    checkpoint 6 work

--- bloc-jams-react/src/App.js
+++ bloc-jams-react/src/App.js
@@ -13,13 +13,14 @@ class App extends Component {
          <nav>
           <Link to='/'>Landing</Link>
           <Link to='/library'>Library</Link>
+          <Link to ='/Album'>Album</Link>
          </nav>
          <h1>Bloc Jams</h1>
         </header>
         <main>
         <Route exact path='/' component={Landing} />
         <Route path='/library' component={Library} />
-        <Route path='/Album:slug' component={Album} />
+        <Route path='/album/:slug' component={Album} />
         </main>
       </div>
     );
